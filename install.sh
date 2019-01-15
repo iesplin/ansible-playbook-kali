d89220d0d40dacbcb84855f3d7c93b01b2f324b6
Ian Esplin <iesplin@users.noreply.github.com>
2019-01-15 16:55:24 +1000

    Set vi mode for tmux conf

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get install -y python3 python3-pip python3-venv
-pip3 install ansible
+pip3 install -U ansible
 ansible-galaxy install -r galaxy-requirements.yml
 ansible-playbook -i inventory kali-playbook.yml
