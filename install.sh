429bbc7931776e3c23360ed257428ec1af146e20
Ian Esplin <iesplin@users.noreply.github.com>
2019-01-21 14:27:18 +1000

    Upgrade pip3 and force ansible role updates

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 apt-get install -y python3 python3-pip python3-venv
+pip3 install --upgrade pip
 pip3 install -U ansible
-ansible-galaxy install -r galaxy-requirements.yml
+ansible-galaxy install --force -r galaxy-requirements.yml
 ansible-playbook -i inventory kali-playbook.yml
