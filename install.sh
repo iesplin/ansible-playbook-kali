cf45b08f34a4f3647ca8b67ee5c78179f2455d23
Ian Esplin <iesplin@users.noreply.github.com>
2019-02-22 15:14:18 +1000

    Project cleanup

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get install -y python3 python3-pip python3-venv
+apt-get install -y python3 python3-pip
 pip3 install --upgrade pip
 pip3 install -U ansible
 ansible-galaxy install --force -r galaxy-requirements.yml
