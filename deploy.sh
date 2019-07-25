ee6f3e615c640862660efbe1489219f0275c7029
iesplin <iesplin@users.noreply.github.com>
2019-07-25 16:29:22 +1000

    Add some sweet ascii art

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+echo " █████╗ ███╗   ██╗███████╗██╗██████╗ ██╗     ███████╗    ██╗  ██╗    ██╗  ██╗ █████╗ ██╗     ██╗"
+echo "██╔══██╗████╗  ██║██╔════╝██║██╔══██╗██║     ██╔════╝    ██║  ██║    ██║ ██╔╝██╔══██╗██║     ██║"
+echo "███████║██╔██╗ ██║███████╗██║██████╔╝██║     █████╗      ███████║    █████╔╝ ███████║██║     ██║"
+echo "██╔══██║██║╚██╗██║╚════██║██║██╔══██╗██║     ██╔══╝      ╚════██║    ██╔═██╗ ██╔══██║██║     ██║"
+echo "██║  ██║██║ ╚████║███████║██║██████╔╝███████╗███████╗         ██║    ██║  ██╗██║  ██║███████╗██║"
+echo "╚═╝  ╚═╝╚═╝  ╚═══╝╚══════╝╚═╝╚═════╝ ╚══════╝╚══════╝         ╚═╝    ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝"
+echo "Manually configuring Kali? Ain't nobody got time for that."
+echo ""
 if [ ! -f "/usr/bin/ansible" ]; then
+    echo "[+] Installing Ansible"
     sudo apt-get install -y ansible
 fi
-
+echo "[+] Downloading latest versions of Ansible roles"
 ansible-galaxy install --force -r galaxy-requirements.yml
+echo "[+] Deploying updates, tools, and config"
 ansible-playbook -i inventory --ask-become-pass kali-playbook.yml
+echo "[+] Deploying user personalizations"
 ansible-playbook -i inventory --ask-become-pass kali-personalize-playbook.yml
+echo "[!] Finished"
