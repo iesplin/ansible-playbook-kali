f099676323ab76c591c7047fbca4bfe3b4358d30
iesplin <iesplin@users.noreply.github.com>
2019-07-26 13:55:14 +1000

    Additional preferences based on ghacks-user.js

--- files/firefox/user.js
+++ files/firefox/user.js
@@ -3,36 +3,130 @@
 // Fix widget behaviour with dark theme
 user_pref("widget.content.gtk-theme-override", "Kali-X");
 
-// Disable New Tab Page
+/* User preferences for quietening Firefox
+ * Based on https://github.com/ghacksuserjs/ghacks-user.js ***/
+/*** [SECTION 0100]: STARTUP ***/
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("browser.startup.page", 0);
+user_pref("browser.startup.homepage", "about:blank");
 user_pref("browser.newtabpage.enabled", false);
-
-// Disable search suggestions
-user_pref("browser.search.suggest.enabled", false);
-
-// Disable telemetry
+user_pref("browser.newtab.preload", false);
 user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
 user_pref("browser.newtabpage.activity-stream.telemetry", false);
-user_pref("browser.pingcentre.telemetry", false);
-user_pref("devtools.onboarding.telemetry-logged", false);
-user_pref("media.wmf.deblacklisting-for-telemetry-in-gpu-process", false);
-user_pref("toolkit.telemetry.archive.enabled", false);
-user_pref("toolkit.telemetry.bhrping.enabled", false);
-user_pref("toolkit.telemetry.firstshutdownping.enabled", false);
-user_pref("toolkit.telemetry.hybridcontent.enabled", false);
-user_pref("toolkit.telemetry.newprofileping.enabled", false);
+user_pref("browser.newtabpage.activity-stream.telemetry.ping.endpoint", "");
+user_pref("browser.newtabpage.activity-stream.asrouter.providers.snippets", "");
+user_pref("browser.newtabpage.activity-stream.feeds.snippets", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket", false);
+user_pref("browser.newtabpage.activity-stream.showSponsored", false);
+user_pref("browser.newtabpage.activity-stream.feeds.discoverystreamfeed", false);
+user_pref("browser.library.activity-stream.enabled", false);
+user_pref("browser.newtabpage.activity-stream.tippyTop.service.endpoint", "");
+
+/*** [SECTION 0200]: GEOLOCATION ***/
+user_pref("permissions.default.geo", 0);
+user_pref("browser.search.region", "US");
+user_pref("browser.search.geoip.url", "");
+user_pref("browser.search.geoSpecificDefaults", false);
+user_pref("browser.search.geoSpecificDefaults.url", "");
+user_pref("intl.accept_languages", "en-US, en");
+user_pref("javascript.use_us_english_locale", true);
+user_pref("intl.regional_prefs.use_os_locales", false);
+user_pref("geo.wifi.uri", "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%");
+user_pref("geo.provider.use_gpsd", false);
+
+/*** [SECTION 0300]: QUIET FOX ***/
+user_pref("app.update.auto", false);
+user_pref("extensions.update.autoUpdateDefault", false);
+user_pref("extensions.getAddons.cache.enabled", false);
+user_pref("browser.search.update", false);
+user_pref("dom.ipc.plugins.flash.subprocess.crashreporter.enabled", false);
+user_pref("dom.ipc.plugins.reportCrashURL", false);
+user_pref("extensions.getAddons.showPane", false);
+user_pref("extensions.webservice.discoverURL", "");
+user_pref("extensions.getAddons.discovery.api_url", "");
+user_pref("extensions.htmlaboutaddons.discover.enabled", false);
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
 user_pref("toolkit.telemetry.unified", false);
-user_pref("toolkit.telemetry.updateping.enabled", false);
-user_pref("toolkit.telemetry.shutdownpingsender.enabled", false);
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.server", "data:,");
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.newProfilePing.enabled", false);
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false);
+user_pref("toolkit.telemetry.bhrPing.enabled", false);
+user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
+user_pref("toolkit.telemetry.hybridContent.enabled", false);
+user_pref("toolkit.telemetry.coverage.opt-out", true);
+user_pref("toolkit.coverage.opt-out", true);
+user_pref("toolkit.coverage.endpoint.base", "");
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("browser.discovery.enabled", false);
+user_pref("breakpad.reportURL", "");
+user_pref("browser.tabs.crashReporting.sendReport", false);
+user_pref("browser.crashReports.unsubmittedCheck.enabled", false);
+user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
+user_pref("extensions.pocket.enabled", false);
+user_pref("captivedetect.canonicalURL", "");
+user_pref("network.captive-portal-service.enabled", false);
+user_pref("network.connectivity-service.enabled", false);
 
-// Disable prefetching
+/*** [SECTION 0400]: BLOCKLISTS / SAFE BROWSING (SB) ***/
+user_pref("extensions.blocklist.enabled", true); // [DEFAULT: true]
+user_pref("extensions.blocklist.url", "https://blocklists.settings.services.mozilla.com/v1/blocklist/3/%APP_ID%/%APP_VERSION%/");
+user_pref("browser.safebrowsing.downloads.remote.enabled", false);
+
+/*** [SECTION 0500]: SYSTEM ADD-ONS / EXPERIMENTS ***/
+user_pref("app.normandy.enabled", false);
+user_pref("app.normandy.api_url", "");
+user_pref("extensions.systemAddon.update.enabled", false);
+user_pref("extensions.systemAddon.update.url", "");
+user_pref("browser.ping-centre.telemetry", false);
+user_pref("extensions.screenshots.disabled", true);
+user_pref("extensions.screenshots.upload-disabled", true);
+user_pref("extensions.formautofill.addresses.enabled", false);
+user_pref("extensions.formautofill.available", "off");
+user_pref("extensions.formautofill.creditCards.enabled", false);
+user_pref("extensions.formautofill.heuristics.enabled", false);
+user_pref("extensions.webcompat-reporter.enabled", false);
+
+/*** [SECTION 0600]: BLOCK IMPLICIT OUTBOUND [not explicitly asked for - e.g. clicked on] ***/
+user_pref("network.prefetch-next", false);
 user_pref("network.dns.disablePrefetch", true);
+user_pref("network.dns.disablePrefetchFromHTTPS", true);
+user_pref("network.predictor.enabled", false);
 user_pref("network.predictor.enable-prefetch", false);
+user_pref("network.http.speculative-parallel-limit", 0);
+user_pref("browser.send_pings", false);
+user_pref("browser.send_pings.require_same_host", true);
 
-// Prevent WebRTC leaking IP addresses
-user_pref("media.peerconnection.ice.no_host", true);
-
-// Don't submit invalid URIs to the default search engine
+/*** [SECTION 0800]: LOCATION BAR / SEARCH BAR / SUGGESTIONS / HISTORY / FORMS ***/
 user_pref("keyword.enabled", false);
-
-// Don't try to guess domain names for invalid domain names
 user_pref("browser.fixup.alternate.enabled", false);
+user_pref("browser.urlbar.trimURLs", false);
+user_pref("browser.sessionhistory.max_entries", 10);
+user_pref("layout.css.visited_links_enabled", false);
+user_pref("browser.search.suggest.enabled", false);
+user_pref("browser.urlbar.suggest.searches", false);
+user_pref("browser.urlbar.usepreloadedtopurls.enabled", false);
+user_pref("browser.urlbar.speculativeConnect.enabled", false);
+user_pref("browser.formfill.enable", false);
+user_pref("browser.taskbar.lists.enabled", false);
+user_pref("browser.taskbar.lists.frequent.enabled", false);
+user_pref("browser.taskbar.lists.recent.enabled", false);
+user_pref("browser.taskbar.lists.tasks.enabled", false);
+user_pref("browser.taskbar.previews.enable", false);
+
+/*** [SECTION 1200]: HTTPS (SSL/TLS / OCSP / CERTS / HPKP / CIPHERS) ***/
+user_pref("security.ssl.errorReporting.automatic", false);
+user_pref("security.ssl.errorReporting.enabled", false);
+user_pref("security.ssl.errorReporting.url", "");
+
+/*** [SECTION 2000]: MEDIA / CAMERA / MIC ***/
+user_pref("media.peerconnection.enabled", false);
+user_pref("media.peerconnection.ice.no_host", true);
+user_pref("media.getusermedia.screensharing.enabled", false);
+user_pref("media.getusermedia.browser.enabled", false);
+user_pref("media.getusermedia.audiocapture.enabled", false);
