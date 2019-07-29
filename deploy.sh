062532c99941ca68351009c9d649c59e12e74aef
iesplin <iesplin@users.noreply.github.com>
2019-07-29 12:11:09 +1000

    Add error handling to deploy script.

--- deploy.sh
+++ deploy.sh
@@ -23,31 +23,48 @@ else
         case $build_selection in
             1* ) kali_build="basic"; break;;
             2* ) kali_build="pro"; break;;
-            * ) echo "Please enter a valid option";;
+            * ) echo "[!] Please enter a valid option.";;
         esac
     done
     echo "kali_build=\"${kali_build}\"" > ${CONF}
 fi
 
 if [ "$kali_build" = basic ] || [ "$kali_build" = pro ]; then
-    echo "Deploying Kali build: ${kali_build}"
+    echo "[+] Deploying Kali build: ${kali_build}"
 else
-    echo "[!] ERROR: Unknown Kali build type. Exiting."
+    echo "[!] Unknown Kali build type: ${kali_build}."
     exit 1
 fi
 
 if [ ! -f "/usr/bin/ansible" ]; then
     echo "[+] Installing Ansible"
     sudo apt-get install -y ansible
+    if [ "$?" -gt 0 ]; then
+        echo "[!] Error occurred when attempting to install Ansible."
+        exit 1
+    fi
 fi
 
 echo -e "\n[+] Downloading latest versions of Ansible roles\n"
 ansible-galaxy install --force -r galaxy-requirements.yml
+if [ "$?" -gt 0 ]; then
+    echo "[!] Error occurred when attempting to download Ansible roles."
+    exit 1    
+fi
 
 echo -e "\n[+] Deploying updates, tools, and config\n"
 ansible-playbook -i inventory-${kali_build} --ask-become-pass kali-playbook.yml
+if [ "$?" -gt 0 ]; then
+    echo "[!] Error occurred during playbook run."
+    exit 1    
+fi
+
 
 echo -e "\n[+] Deploying user personalizations\n"
 ansible-playbook -i inventory-${kali_build} --ask-become-pass kali-personalize-playbook.yml
+if [ "$?" -gt 0 ]; then
+    echo "[!] Error occurred during playbook run."
+    exit 1    
+fi
 
 echo "[!] Finished"
