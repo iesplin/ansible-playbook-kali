6f0a0525edf620b1c01f1b7b58414fde6d816520
iesplin <iesplin@users.noreply.github.com>
2019-10-25 09:56:26 +1000

    Refactoring of playbooks

--- deploy.sh
+++ deploy.sh
@@ -10,59 +10,38 @@ echo " ╚═╝  ╚═╝╚═╝  ╚═══╝╚══════╝╚
 echo " Manually configuring Kali? Ain't nobody got time for that."
 echo ""
 
-CONF="./deploy.conf"
-
-if [ -f "${CONF}" ]; then
-    source ${CONF}
-else
-    echo "Which Kali build do want to deploy?"
-    echo "[1] Basic - Burp Suite Community"
-    echo "[2] Pro   - Burp Suite Pro"
-    echo ""
-    while true; do
-        read -p "Enter the number for the build: " build_selection
-        case $build_selection in
-            1* ) kali_build="basic"; break;;
-            2* ) kali_build="pro"; break;;
-            * ) echo "[!] Please enter a valid option.";;
-        esac
-    done
-    echo "kali_build=\"${kali_build}\"" > ${CONF}
-fi
-
-if [ "$kali_build" = basic ] || [ "$kali_build" = pro ]; then
-    echo "[+] Deploying Kali build: ${kali_build}"
-else
-    echo "[!] Unknown Kali build type: ${kali_build}."
-    exit 1
-fi
-
-if [ ! -f "/usr/bin/ansible" ]; then
-    echo "[+] Installing Ansible"
-    sudo apt-get install -y ansible
+if ! command -v pip3; then
+    echo "[+] Installing Pip for Python 3"
+    sudo apt-get install -y python3-pip python3-setuptools
     if [ "$?" -gt 0 ]; then
-        echo "[!] Error occurred when attempting to install Ansible."
+        echo "[!] Error occurred when attempting to install Pip."
+        exit 1
+    fi
+fi
+
+if ! command -v pipenv; then
+    pip3 install --user pipenv
+    if [ "$?" -gt 0 ]; then
+        echo "[!] Error occurred when attempting to install Pipenv."
+        exit 1
+    fi
+elif
+    pipenv --three install
+    pipenv update
+    if [ "$?" -gt 0 ]; then
+        echo "[!] Error occurred when attempting to update Pipenv."
         exit 1
     fi
 fi
 
 echo -e "\n[+] Downloading latest versions of Ansible roles\n"
-ansible-galaxy install --force -r galaxy-requirements.yml
+#ansible-galaxy install --force -r galaxy-requirements.yml
 if [ "$?" -gt 0 ]; then
     echo "[!] Error occurred when attempting to download Ansible roles."
     exit 1    
 fi
 
-echo -e "\n[+] Deploying updates, tools, and config\n"
-ansible-playbook -i inventory-${kali_build} --ask-become-pass kali-playbook.yml
-if [ "$?" -gt 0 ]; then
-    echo "[!] Error occurred during playbook run."
-    exit 1    
-fi
-
-
-echo -e "\n[+] Deploying user personalizations\n"
-ansible-playbook -i inventory-${kali_build} kali-personalize-playbook.yml
+pipenv run ansible-playbook -i inventory --ask-become-pass main.yml
 if [ "$?" -gt 0 ]; then
     echo "[!] Error occurred during playbook run."
     exit 1    
