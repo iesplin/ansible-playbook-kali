80545ea585edf8857a8de24e024b222319a813d0
iesplin <iesplin@users.noreply.github.com>
2019-10-25 10:21:24 +1000

    Update Ansible Galaxy requirements

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ echo " ╚═╝  ╚═╝╚═╝  ╚═══╝╚══════╝╚
 echo " Manually configuring Kali? Ain't nobody got time for that."
 echo ""
 
-if ! command -v pip3; then
+if ! command -v pip3 > /dev/null; then
     echo "[+] Installing Pip for Python 3"
     sudo apt-get install -y python3-pip python3-setuptools
     if [ "$?" -gt 0 ]; then
@@ -19,8 +19,8 @@ if ! command -v pip3; then
     fi
 fi
 
-if ! command -v pipenv; then
-    pip3 install --user pipenv
+if ! command -v pipenv > /dev/null; then
+    pip3 install --user --upgrade pipenv
     if [ "$?" -gt 0 ]; then
         echo "[!] Error occurred when attempting to install Pipenv."
         exit 1
