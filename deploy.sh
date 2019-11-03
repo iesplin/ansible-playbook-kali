5fca706b9391d8c6b5a1a77c701e5ee7833fd10d
iesplin <iesplin@users.noreply.github.com>
2019-11-04 09:26:34 +1000

    Merge changes from dev branch (#1)
    * Refactoring of playbooks
    
    * Fix script error
    
    * Update deploy script
    
    * Update Ansible Galaxy requirements
    
    * Check for existing venv
    
    * Fix deploy script logic
    
    * Hide venv check output
    
    * Fix pipenv run commands
    
    * Update playbooks
    
    * Fix playbooks
    
    * Update main playbook
    
    * Update main playbook
    
    * Update deploy script
    
    * Removed using a venv for Ansible
    
    * Fix undefined variable
    
    * Always install/update Ansible
    
    * Update deploy script
    
    * New role for Keystone Engine due to pypi package issues
    
    * Update deploy script
    
    * Check packages installed and use virtualenv
    
    * Fix if statements
    
    * Remove unnecessary quotes
    
    * Fix incorrect check
    
    * Update deploy script
    
    * Hide dpkg output
    
    * Update deploy script
    
    * Remove virtualenv due to conflicts

--- deploy.sh
+++ deploy.sh
@@ -10,37 +10,25 @@ echo " ╚═╝  ╚═╝╚═╝  ╚═══╝╚══════╝╚
 echo " Manually configuring Kali? Ain't nobody got time for that."
 echo ""
 
-CONF="./deploy.conf"
+required_packages=( python3-pip )
+missing_packages=""
 
-if [ -f "${CONF}" ]; then
-    source ${CONF}
-else
-    echo "Which Kali build do want to deploy?"
-    echo "[1] Basic - Burp Suite Community"
-    echo "[2] Pro   - Burp Suite Pro"
-    echo ""
-    while true; do
-        read -p "Enter the number for the build: " build_selection
-        case $build_selection in
-            1* ) kali_build="basic"; break;;
-            2* ) kali_build="pro"; break;;
-            * ) echo "[!] Please enter a valid option.";;
-        esac
-    done
-    echo "kali_build=\"${kali_build}\"" > ${CONF}
+for deb in "${required_packages[@]}"; do
+    dpkg --get-selections| grep -E "^$deb\s+install$" 2>&1 >/dev/null
+    if [ $? -eq 1 ]; then
+        missing_packages+="$deb "
+    fi
+done
+
+if [ ! -z "$missing_packages" ]; then
+    echo "[+] Installing required packages"
+    sudo apt-get install -y $missing_packages
 fi
 
-if [ "$kali_build" = basic ] || [ "$kali_build" = pro ]; then
-    echo "[+] Deploying Kali build: ${kali_build}"
-else
-    echo "[!] Unknown Kali build type: ${kali_build}."
-    exit 1
-fi
-
-if [ ! -f "/usr/bin/ansible" ]; then
+if ! command -v ansible; then
     echo "[+] Installing Ansible"
-    sudo apt-get install -y ansible
-    if [ "$?" -gt 0 ]; then
+    sudo -H pip3 install --upgrade ansible
+    if [ $? -gt 0 ]; then
         echo "[!] Error occurred when attempting to install Ansible."
         exit 1
     fi
@@ -48,22 +36,14 @@ fi
 
 echo -e "\n[+] Downloading latest versions of Ansible roles\n"
 ansible-galaxy install --force -r galaxy-requirements.yml
-if [ "$?" -gt 0 ]; then
+if [ $? -gt 0 ]; then
     echo "[!] Error occurred when attempting to download Ansible roles."
     exit 1    
 fi
 
-echo -e "\n[+] Deploying updates, tools, and config\n"
-ansible-playbook -i inventory-${kali_build} --ask-become-pass kali-playbook.yml
-if [ "$?" -gt 0 ]; then
-    echo "[!] Error occurred during playbook run."
-    exit 1    
-fi
-
-
-echo -e "\n[+] Deploying user personalizations\n"
-ansible-playbook -i inventory-${kali_build} kali-personalize-playbook.yml
-if [ "$?" -gt 0 ]; then
+echo -e "\n[+] Running Ansible 4 Kali playbooks\n"
+ansible-playbook -i inventory --ask-become-pass main.yml
+if [ $? -gt 0 ]; then
     echo "[!] Error occurred during playbook run."
     exit 1    
 fi
